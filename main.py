9ba79c5b3bc739984dbe25ba02a92049b3db12f2
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-07-08 23:27:32 -0400

    Finished setting up the sending of the email if the album count changes, indicating there is new music and notifying the specified email

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 import json
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # import urllib.request
 import requests
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
+
+
 
 load_dotenv()
 
@@ -20,10 +25,41 @@ CLIENT_SECRET = os.getenv("APP_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 SCOPE = os.getenv("SCOPE")
 
-SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
-SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
-SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1'
 
+def sendEmail(bands):
+  EMAIL_USERNAME = os.getenv("EMAIL_USERNAME")
+  EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+  TO_EMAIL = os.getenv("TO_EMAIL")
+  FROM_EMAIL = os.getenv("FROM_EMAIL")
+  
+  email_subject = "New music from a band you like!"
+  body = "Looks like there's new music from {bands}".format(bands=bands)
+  
+  msg = MIMEMultipart()
+  msg['From'] = FROM_EMAIL
+  msg['To'] = TO_EMAIL
+  msg['Subject'] = email_subject
+
+  msg.attach(MIMEText(body, 'plain'))
+
+  try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+
+    text = msg.as_string()
+    server.sendmail(FROM_EMAIL, TO_EMAIL, text)
+    server.quit()
+
+    print("Email sent!")
+  except Exception as e:
+    print(f"Failed to send email: {e}")
+
+
+
+
+
+  
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE, 
                                                client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
@@ -34,6 +70,7 @@ with open('BandList.json', 'r') as file:
   jsonData = json.load(file)
 
 #updating the album count if its changed
+newMusic = []
 for band in jsonData["bands"]:
   results = sp.search(q=band["name"], type='artist')
   res = sp.artist_albums(results["artists"]["items"][0]['uri'], include_groups='album,single', limit=20)
@@ -49,21 +86,13 @@ for band in jsonData["bands"]:
       isNext = False
   if(count != band["albums"]):
     band["albums"] = count
+    newMusic.append(band["name"])
+
+if(len(newMusic) != 0):
+  sendEmail(newMusic)
 
 # write data back into the json file
 with open('BandList.json', 'w') as file:
   json.dump(jsonData, file, indent=2)
 
 print("data has been updated")
-
-
-# taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
-
-# results = sp.artist_albums(taylor_uri, album_type='album')
-# albums = results['items']
-# while results['next']:
-#     results = sp.next(results)
-#     albums.extend(results['items'])
-
-# for album in albums:
-#     print(album['name'])
